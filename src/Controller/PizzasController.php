a34df14b919c36659a2ac8d7b0db70e20bd3e6db
Viktor Chegodaev <vitas1648@gmail.com>
2021-12-20 22:17:30 +0500

    Update Pizza controller, twig templates

--- src/Controller/PizzasController.php
+++ src/Controller/PizzasController.php
@@ -2,17 +2,30 @@
 
 namespace App\Controller;
 
+use App\Entity\Pizza;
+use App\Repository\PizzaRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
 
 class PizzasController extends AbstractController
 {
     #[Route('/pizzas', name: 'pizzas')]
-    public function index(): Response
+    public function index(Environment $twig, PizzaRepository $pizzaRepository): Response
     {
-        return $this->render('pizzas/index.html.twig', [
-            'controller_name' => 'PizzasController',
-        ]);
+        return new Response($twig->render('pizzas/index.html.twig', [
+            'pizzas' => $pizzaRepository->findAll(),
+        ]));
     }
+
+    #[Route('/pizza/{id}', name: 'pizza')]
+    public function aboutPizza(Environment $twig, Pizza $pizza, 
+        PizzaRepository $pizzaRepository): Response
+    {
+        return new Response($twig->render('pizzas/aboutPizza.html.twig', [
+            'pizza' => $pizza,
+        ]));
+    }
+
 }
