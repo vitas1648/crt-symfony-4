f9e66720de2437205e6622167ad8433ed218b0fa
Viktor Chegodaev <vitas1648@gmail.com>
2021-12-20 15:48:45 +0500

    Update ingredientsController, output all ingredients

--- src/Controller/IngredientsController.php
+++ src/Controller/IngredientsController.php
@@ -2,17 +2,23 @@
 
 namespace App\Controller;
 
+use App\Entity\Ingredient;
+use App\Repository\IngredientRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
 
 class IngredientsController extends AbstractController
 {
     #[Route('/ingredients', name: 'ingredients')]
-    public function index(): Response
+    public function index(Environment $twig, IngredientRepository $ingredientRepository): Response
     {
-        return $this->render('ingredients/index.html.twig', [
-            'controller_name' => 'IngredientsController',
-        ]);
+        return new Response($twig->render('ingredients/index.html.twig', [
+            'ingredients' => $ingredientRepository->findAll(),
+        ]));
+//        return $this->render('ingredients/index.html.twig', [
+//            'controller_name' => 'IngredientsController',
+//        ]);
     }
 }
