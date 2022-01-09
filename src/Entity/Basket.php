33ce84c25ffc352bb92de2f906c8750d5fdec067
Viktor Chegodaev <vitas1648@gmail.com>
2022-01-10 01:26:48 +0500

    Added Basket, Customer API

--- src/Entity/Basket.php
+++ src/Entity/Basket.php
@@ -2,12 +2,14 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Core\Annotation\ApiResource;
 use App\Repository\BasketRepository;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
  * @ORM\Entity(repositoryClass=BasketRepository::class)
  */
+#[ApiResource()]
 class Basket
 {
     /**
